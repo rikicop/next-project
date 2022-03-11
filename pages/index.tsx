56b2810229efb18730e6776cc69695ca74d867f3
rikicop <ruperto1@protonmail.com>
2022-03-11 10:57:44 -0500

    Second

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,56 @@
-import styled from 'styled-components'
+import { GetStaticProps } from "next";
+import styled from "styled-components";
+import ArticleList from "../components/ArticleList";
+import { server } from "../config";
+import { IndexPageProps } from "../Interfaces";
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+const Hero = styled.div`
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+`;
 
-export default function Home() {
-  return <Title>My page</Title>
+const Heading = styled.h1`
+  color: #000;
+  font-size: 10rem;
+  font-weight: 900;
+`;
+
+export default function Home({ data }: IndexPageProps) {
+  //console.log(data);
+  return (
+    <>
+      <Hero>
+        <Heading>NEXT</Heading>
+      </Hero>
+      <ArticleList data={data} />
+    </>
+  );
 }
+
+//getStaticProps
+// export const getStaticProps: GetStaticProps = async () => {
+//   const res = await fetch(
+//     "https://jsonplaceholder.typicode.com/posts?_limit=8"
+//   );
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// };
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
